3851e7b980c72957ecf5b5459b291529a03cda3c
kalashnikovsb <kalashnikov.sk8@gmail.com>
2019-12-12 00:32:06 +0700

    Делаю скрипты

--- build/js/script.js
+++ build/js/script.js
@@ -1,15 +1,30 @@
 'use strict';
-var pageHeader = document.querySelector('.page-header');
-var headerToggle = document.querySelector('.page-header__toggle');
 
-pageHeader.classList.remove('page-header--nojs');
+var popup = document.querySelector('.popup');
+var form = document.querySelector('popup__form');
+var openButton = document.querySelector('.button--callback');
+var closeButton = document.querySelector('.popup__close');
+var name = document.querySelector('.popup__input--name');
+var tel = document.querySelector('.popup__input--tel');
 
-headerToggle.addEventListener('click', function () {
-  if (pageHeader.classList.contains('page-header--closed')) {
-    pageHeader.classList.remove('page-header--closed');
-    pageHeader.classList.add('page-header--opened');
-  } else {
-    pageHeader.classList.add('page-header--closed');
-    pageHeader.classList.remove('page-header--opened');
+console.log(name);
+
+openButton.addEventListener('click', function (evt) {
+  evt.preventDefault();
+  if (popup.classList.contains('visually-hidden')) {
+    popup.classList.remove('visually-hidden');
+  }
+  name.focus();
+});
+
+closeButton.addEventListener('click', function () {
+  if (!popup.classList.contains('visually-hidden')) {
+    popup.classList.add('visually-hidden');
+  }
+});
+
+window.addEventListener('keydown', function (evt) {
+  if (evt.keyCode === 27) {
+    popup.classList.add('visually-hidden');
   }
 });
