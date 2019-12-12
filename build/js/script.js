20415d769596d6280b166dd948942d636258060b
kalashnikovsb <kalashnikov.sk8@gmail.com>
2019-12-12 22:35:51 +0700

    Скрипты

--- build/js/script.js
+++ build/js/script.js
@@ -1,5 +1,6 @@
 'use strict';
 
+var body = document.querySelector('body');
 var popup = document.querySelector('.popup');
 var form = document.querySelector('popup__form');
 var openButton = document.querySelector('.button--callback');
@@ -7,10 +8,14 @@ var closeButton = document.querySelector('.popup__close');
 var name = document.querySelector('.popup__input--name');
 var tel = document.querySelector('.popup__input--tel');
 
+var overlay = document.createElement('div');
+overlay.classList.add('overlay');
+
 openButton.addEventListener('click', function (evt) {
   evt.preventDefault();
   if (popup.classList.contains('visually-hidden')) {
     popup.classList.remove('visually-hidden');
+    body.appendChild(overlay);
   }
   name.focus();
 });
@@ -18,11 +23,21 @@ openButton.addEventListener('click', function (evt) {
 closeButton.addEventListener('click', function () {
   if (!popup.classList.contains('visually-hidden')) {
     popup.classList.add('visually-hidden');
+    body.removeChild(overlay);
   }
 });
 
 window.addEventListener('keydown', function (evt) {
   if (evt.keyCode === 27) {
     popup.classList.add('visually-hidden');
+    body.removeChild(overlay);
   }
+
+  window.addEventListener('click', function () {
+    if (!popup.classList.contains('visually-hidden')) {
+      popup.classList.add('visually-hidden');
+      body.removeChild(overlay);
+    }
+  });
+
 });
--- source/js/script.js
+++ source/js/script.js
@@ -2,30 +2,25 @@
 
 var body = document.querySelector('body');
 var popup = document.querySelector('.popup');
-var form = document.querySelector('popup__form');
 var openButton = document.querySelector('.button--callback');
 var closeButton = document.querySelector('.popup__close');
 var name = document.querySelector('.popup__input--name');
-var tel = document.querySelector('.popup__input--tel');
 
+var overlay = document.createElement('div');
+overlay.classList.add('overlay');
 
 openButton.addEventListener('click', function (evt) {
   evt.preventDefault();
   if (popup.classList.contains('visually-hidden')) {
     popup.classList.remove('visually-hidden');
+    body.appendChild(overlay);
   }
-
   name.focus();
 });
 
 closeButton.addEventListener('click', function () {
   if (!popup.classList.contains('visually-hidden')) {
     popup.classList.add('visually-hidden');
-  }
-});
-
-window.addEventListener('keydown', function (evt) {
-  if (evt.keyCode === 27) {
-    popup.classList.add('visually-hidden');
+    body.removeChild(overlay);
   }
 });
