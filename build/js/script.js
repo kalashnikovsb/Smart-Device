e6bc4b31264085b8e70d9cd9ce049d94ba6df1a8
kalashnikovsb <kalashnikov.sk8@gmail.com>
2019-12-13 20:29:04 +0700

    Добавил localStorage

--- build/js/script.js
+++ build/js/script.js
@@ -2,11 +2,17 @@
 
 var body = document.querySelector('body');
 var popup = document.querySelector('.popup');
+var form = document.querySelector('.popup__form');
 var openButton = document.querySelector('.button--callback');
 var closeButton = document.querySelector('.popup__close');
+var nameInput = document.querySelector('.popup__input--name');
+var telInput = document.querySelector('.popup__input--tel');
+var textarea = document.querySelector('.popup__textarea');
 
 var overlay = document.createElement('div');
 overlay.classList.add('overlay');
+
+// Открытие попапа:
 
 openButton.addEventListener('click', function (evt) {
   evt.preventDefault();
@@ -14,7 +20,20 @@ openButton.addEventListener('click', function (evt) {
     popup.classList.remove('visually-hidden');
     body.appendChild(overlay);
   }
+
+  if (localStorage.getItem('name')) {
+    nameInput.value = localStorage.getItem('name');
+    telInput.focus();
+  }
+  if (localStorage.getItem('tel')) {
+    telInput.value = localStorage.getItem('tel');
+    textarea.focus();
+  } else {
+    nameInput.focus();
+  }
 });
+
+// Закрытие попапа:
 
 closeButton.addEventListener('click', function () {
   if (!popup.classList.contains('visually-hidden')) {
@@ -35,4 +54,16 @@ overlay.addEventListener('click', function () {
     popup.classList.add('visually-hidden');
     body.removeChild(overlay);
   }
+});
+
+// Отправка формы:
+
+form.addEventListener('submit', function (evt) {
+  if (!nameInput.value || !telInput.value) {
+    evt.preventDefault();
+    popup.classList.add('visually-hidden');
+    body.removeChild(overlay);
+  }
+  localStorage.setItem('name', nameInput.value);
+  localStorage.setItem('tel', telInput.value);
 });
